57bf3276d64d117c4193445bfaeede9a302e5b5a
amidou.zabre <amidou.zabre@gmail.com>
2025-03-10 14:21:39 +0000

    add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+//import type { Config } from "tailwindcss";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'airbnb': "#ff385c",
+        'airbnb-dark': "d50027"
+      }
+    },
+  },
+  plugins: [],
+}
+
