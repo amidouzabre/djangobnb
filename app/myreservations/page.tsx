27517f462de160db3ee8e5f8cc7b0f1ee20a4ddf
amidou.zabre <amidou.zabre@gmail.com>
2025-03-12 16:45:09 +0000

    My reservations: details

--- app/myreservations/page.tsx
+++ app/myreservations/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function MyReservationPage () {
     return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6">
-            <h1 className="mt-6 mb-2 text-2xl">My reservations</h1>
-            <div>
-                <div className="p-5 mt-4 grid grid-cols-4 gap-4 shadow-md border border-gray-300 rounded-xl">
+            <h1 className="my-6 text-2xl">My reservations</h1>
+            <div className="space-y-4">
+                <div className="p-5 grid grid-cols-1 md:grid-cols-4 gap-4 shadow-md border border-gray-300 rounded-xl">
                     
                     <div className="col-span-1">
                         <div className="relative overflow-hidden aspect-square rounded-xl">
@@ -19,12 +19,79 @@ export default function MyReservationPage () {
                         </div>
                     </div>
 
-                    <div className="col-span-3 space-y-2">  
+                    <div className=" col-span-1 md:col-span-3">  
                         <h2 className="mb-4 text-xl">Property name</h2>
-                        <p><strong>Check in date : </strong>14/02/2024</p>
+                        <p className="mb-2"><strong>Check in date : </strong>14/02/2024</p>
+                        <p className="mb-2"><strong>Check out date : </strong>16/02/2024</p>
+                        <p className="mb-2"><strong>Number of nights : </strong>2</p>
+                        <p className="mb-2"><strong>Total price : </strong>$200</p>
+
+                        <div className="mt-6 cursor-pointer md:inline-block block py-3 px-6 bg-airbnb hover:bg-airbnb-dark text-white rounded-xl transition">
+                            Go to property
+                        </div>
+                    </div>
+
+                    
+
+                </div>
+
+                <div className="p-5 grid grid-cols-1 md:grid-cols-4 gap-4 shadow-md border border-gray-300 rounded-xl">
+                    
+                    <div className="col-span-1">
+                        <div className="relative overflow-hidden aspect-square rounded-xl">
+                            <Image 
+                                src='/beach_03.avif'
+                                alt="Piscine house"
+                                loading="lazy"
+                                fill={true}
+                                className="hover:scale-110 object-cover transition h-full"
+                            />
+                        </div>
+                    </div>
+
+                    <div className=" col-span-1 md:col-span-3">  
+                        <h2 className="mb-4 text-xl">Property name</h2>
+                        <p className="mb-2"><strong>Check in date : </strong>14/02/2024</p>
+                        <p className="mb-2"><strong>Check out date : </strong>16/02/2024</p>
+                        <p className="mb-2"><strong>Number of nights : </strong>2</p>
+                        <p className="mb-2"><strong>Total price : </strong>$200</p>
+
+                        <div className="mt-6 cursor-pointer md:inline-block block py-3 px-6 bg-airbnb hover:bg-airbnb-dark text-white rounded-xl transition">
+                            Go to property
+                        </div>
                     </div>
 
                 </div>
+
+                <div className="p-5 grid grid-cols-1 md:grid-cols-4 gap-4 shadow-md border border-gray-300 rounded-xl">
+                    
+                    <div className="col-span-1">
+                        <div className="relative overflow-hidden aspect-square rounded-xl">
+                            <Image 
+                                src='/beach_02.avif'
+                                alt="Piscine house"
+                                loading="lazy"
+                                fill={true}
+                                className="hover:scale-110 object-cover transition h-full"
+                            />
+                        </div>
+                    </div>
+
+                    <div className=" col-span-1 md:col-span-3">  
+                        <h2 className="mb-4 text-xl">Property name</h2>
+                        <p className="mb-2"><strong>Check in date : </strong>14/02/2024</p>
+                        <p className="mb-2"><strong>Check out date : </strong>16/02/2024</p>
+                        <p className="mb-2"><strong>Number of nights : </strong>2</p>
+                        <p className="mb-2"><strong>Total price : </strong>$200</p>
+
+                        <div className="mt-6 cursor-pointer md:inline-block block py-3 px-6 bg-airbnb hover:bg-airbnb-dark text-white rounded-xl transition">
+                            Go to property
+                        </div>
+                    </div>
+
+                </div>
+
+
             </div>
         </main>
     )
