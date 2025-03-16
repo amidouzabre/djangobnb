2c17cfc70e2fecebbf06a130ab567ad1153a5c88
amidou.zabre <amidou.zabre@gmail.com>
2025-03-16 12:47:12 +0000

    Create Propety api

---
+++ backend/djangobnb_backend/property/api.py
@@ -0,0 +1,15 @@
+from django.http import JsonResponse
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+
+from .models import Property
+from .serializers import PropertyListSerializer
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def property_list(request):
+    properties = Property.objects.all()
+    serializer = PropertyListSerializer(properties, many=True)
+    return JsonResponse({
+        'data': serializer.data,
+    })
